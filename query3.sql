3e07d4706c0d305b86c3c19420aaeffc425418d6
Kommerla <bharatchandrakommerla01.cb@gmail.com>
2021-06-18 14:16:56 +0530

    Third Commit

--- query3.sql
+++ query3.sql
@@ -1,1 +1,1 @@
-SELECT book FROM books_subjects WHERE books_subjects.book=books.title AND books_subjects.subject IN (3,8);
+SELECT b.title FROM books b, books_subjects bs WHERE bs.book=b.id AND bs.subject IN (3,8);
